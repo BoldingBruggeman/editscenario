a000c9471afe615b8b043a9de8731fec0d17f43f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2019-06-19 11:37:33 +0100

    towards python3 compatibility

--- editscenario/editscenario.py
+++ editscenario/editscenario.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-
+from __future__ import print_function
 import sys, os, os.path
 
 # Add xmlstore and xmlplot directories to the search path.
@@ -65,10 +65,11 @@ def main():
     except:
         # Check if the source path exist.
         if not os.path.isfile(valuespath):
-            print 'Error! The XML values file "%s" does not exist.' % valuespath
+            print('Error! The XML values file "%s" does not exist.' % valuespath)
             return 1
         scen = Scenario.fromXmlFile(valuespath,targetstore=targetstore)
-    if options.verbose: print 'Operating on values file with version %s.' % scen.version
+    if options.verbose:
+        print('Operating on values file with version %s.' % scen.version)
     
     context = {'container':xmlstore.datatypes.DataContainerDirectory(os.getcwd())}
     def processAssignments(assignments,ignoremissing=False,quiet=False):
@@ -76,7 +77,8 @@ def main():
             node = scen.findNode(name)
             if node is None:
                 if ignoremissing: continue
-                if options.verbose: print 'Node "%s" was not found in scenario.' % name
+                if options.verbose:
+                    print('Node "%s" was not found in scenario.' % name)
                 return False
                 
             fullname = '/'.join(node.location)
@@ -84,11 +86,12 @@ def main():
             tp = node.getValueType(returnclass=True)
             try:
                 val = tp.fromXmlString(val,context,node.templatenode)
-                if not quiet: print '"%s": assigning value "%s".' % (fullname,val.toPrettyString())
+                if not quiet:
+                    print('"%s": assigning value "%s".' % (fullname,val.toPrettyString()))
                 node.setValue(val)
                 if isinstance(val,xmlstore.util.referencedobject): val.release()
-            except Exception,e:
-                print '"%s": cannot assign value "%s". %s' % (fullname,val,e)
+            except Exception as e:
+                print('"%s": cannot assign value "%s". %s' % (fullname,val,e))
         return True
 
     # Process environment-based assignments
@@ -99,7 +102,7 @@ def main():
         assignments = []
         for item in options.assignments:
             if '=' not in item:
-                print 'Error: "%s" is not a valid assignment. Assignments must follow the pattern VARIABLE=VALUE.' % item
+                print('Error: "%s" is not a valid assignment. Assignments must follow the pattern VARIABLE=VALUE.' % item)
                 return 2
             assignments.append(item.split('=',1))
         if not processAssignments(assignments,quiet=not options.verbose): return 2
@@ -109,18 +112,18 @@ def main():
     if options.root is not None:
         rootnode = scen[options.root]
         if rootnode is None:
-            print 'Node "%s" was not found in schema.' % options.root
+            print('Node "%s" was not found in schema.' % options.root)
             sys.exit(2)
         node2role = scen.detectNodeRolesInNamelist()
         if options.gui and len(rootnode.children)==0:
-            print 'Node "%s" contains no child nodes. Such nodes cannot be used as root if -g/--gui is specified.' % '/'.join(rootnode.location)
+            print('Node "%s" contains no child nodes. Such nodes cannot be used as root if -g/--gui is specified.' % '/'.join(rootnode.location))
             sys.exit(2)
         if options.export=='nml':
             if node2role[rootnode]==2:
-                print 'Node "%s" represents a namelist, rather than a directory or file. Such nodes cannot be used as root if --export nml is specified.' % '/'.join(rootnode.location)
+                print('Node "%s" represents a namelist, rather than a directory or file. Such nodes cannot be used as root if --export nml is specified.' % '/'.join(rootnode.location))
                 sys.exit(2)
             elif node2role[rootnode]==3:
-                print 'Node "%s" represents a namelist variable, rather than a directory or file. Such nodes cannot be used as root if --export nml is specified.' % '/'.join(rootnode.location)
+                print('Node "%s" represents a namelist variable, rather than a directory or file. Such nodes cannot be used as root if --export nml is specified.' % '/'.join(rootnode.location))
                 sys.exit(2)
 
     if options.gui:
@@ -140,32 +143,38 @@ def main():
         if rootnode is not None: nodes = rootnode.getDescendants()
         errors = scen.validate(nodes)
         if errors:
-            print 'Validation failed! Errors:'
-            for error in errors: print error
+            print('Validation failed! Errors:')
+            for error in errors:
+                print(error)
             if options.export:
-                print 'Nothing done because scenario validation failed (specify --skipvalidation if you want to ignore validation errors).'
+                print('Nothing done because scenario validation failed (specify --skipvalidation if you want to ignore validation errors).')
                 return 1
         elif options.verbose:
             if options.root is not None:
-                print 'Scenario subset %s validated successfully.' % '/'.join(rootnode.location)
+                print('Scenario subset %s validated successfully.' % '/'.join(rootnode.location))
             else:
-                print 'Scenario validated successfully.'
+                print('Scenario validated successfully.')
 
     # Export the scenario
     if exportformat=='nml':
-        if options.verbose: print 'Exporting values to namelist file(s) %s...' % targetpath
+        if options.verbose:
+            print('Exporting values to namelist file(s) %s...' % targetpath)
         scen.writeAsNamelists(targetpath,addcomments=True,allowmissingvalues=True,root=options.root)
     elif exportformat=='xml':
-        if options.verbose: print 'Exporting values to XML-based values file (%s)...' % targetpath
+        if options.verbose:
+            print('Exporting values to XML-based values file (%s)...' % targetpath)
         scen.save(targetpath)
     elif exportformat=='dir':
-        if options.verbose: print 'Exporting packaged values to directory (%s)...' % targetpath
+        if options.verbose:
+            print('Exporting packaged values to directory (%s)...' % targetpath)
         scen.saveAll(targetpath,targetisdir=True)
     elif exportformat=='zip':
-        if options.verbose: print 'Exporting packaged values to ZIP file (%s)...' % targetpath
+        if options.verbose:
+            print('Exporting packaged values to ZIP file (%s)...' % targetpath)
         scen.saveAll(targetpath)
     else:
-        if options.verbose: print 'No further action taken. Use -e to export setup.'
+        if options.verbose:
+            print('No further action taken. Use -e to export setup.')
 
     # Clean-up (delete temporary directories etc.)
     scen.release()
@@ -173,6 +182,6 @@ def main():
     return 0
 
 # If the script has been run (as opposed to imported), enter the main loop.
-if (__name__=='__main__'):
+if __name__ == '__main__':
     ret = main()
     sys.exit(ret)
