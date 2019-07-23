ba29952cd612b45829bf3d4cd039ccc0792c0307
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2019-07-23 14:53:21 +0100

    updated xmlstore dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='editscenario',
-      version='0.11.3',
+      version='0.11.4',
       description='Generate Fortran namelists from XML-files',
       long_description=readme(),
       url='http://github.com/BoldingBruggeman/editscenario',
@@ -27,6 +27,6 @@ setup(name='editscenario',
       },
       packages=['editscenario'],
 #      packages=find_packages(exclude=['bin']),
-      install_requires=['xmlstore>=0.9.5'],
+      install_requires=['xmlstore>=0.9.6'],
       include_package_data=True,
       zip_safe=False)
